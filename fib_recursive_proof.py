2fda7468503bb5d6b3941d44ff4330daf180f7e8
guanqg123 <36056622+guanqg123@users.noreply.github.com>
2018-03-23 15:22:08 -0500

    Update fib_recursive_proof.py

--- fib_recursive_proof.py
+++ fib_recursive_proof.py
@@ -8,27 +8,27 @@ print("Following is fib(n) = fib(n-1)+fib(n-2)")
 #store   fib(2)+fib(1)
 #call    fib(2)
 #store   fib(1)+fib(0)
-#call    fib(1)       #has return value
-#call    fib(0)       #has return value
-#compute fib(1)+fib(0)#return the value to fib(2)
-#call    fib(1)       #has return value
-#compute fib(2)+fib(1)#return the value to fib(3)
+#call    fib(1)        #has return value
+#call    fib(0)        #has return value
+#compute fib(1)+fib(0) #return the value to fib(2)
+#call    fib(1)        #has return value
+#compute fib(2)+fib(1) #return the value to fib(3)
 #call    fib(2)
 #store   fib(1)+fib(0)
-#call    fib(1)       #has return value
-#call    fib(0)       #has return value
-#compute fib(1)+fib(0)#return the value to fib(2)
-#compute fib(3)+fib(2)#return the value to fib(4)
+#call    fib(1)        #has return value
+#call    fib(0)        #has return value
+#compute fib(1)+fib(0) #return the value to fib(2)
+#compute fib(3)+fib(2) #return the value to fib(4)
 #call    fib(3)
 #store   fib(2)+fib(1)
 #call    fib(2)
 #store   fib(1)+fib(0)
-#call    fib(1)       #has return value
-#call    fib(0)       #has return value
-#compute fib(1)+fib(0)#return the value to fib(2)
-#call    fib(1)       #has return value
-#compute fib(2)+fib(1)#return the value to fib(3)
-#compute fib(4)+fib(3)#return the value to fib(5)
+#call    fib(1)        #has return value
+#call    fib(0)        #has return value
+#compute fib(1)+fib(0) #return the value to fib(2)
+#call    fib(1)        #has return value
+#compute fib(2)+fib(1) #return the value to fib(3)
+#compute fib(4)+fib(3) #return the value to fib(5)
 
 def fib(n):
     print("call  fib({})".format(n))
